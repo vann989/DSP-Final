10b8867a9f7f57cbdb3b6545f393d77d5ef5d559
vann989 <131186490+vann989@users.noreply.github.com>
2023-05-01 10:57:05 -0600

    Update DFTimg.py

--- DFTimg.py
+++ DFTimg.py
@@ -106,7 +106,47 @@ class DFT():
         plt.imshow(self.img)
         plt.show()
 
+    def compress_image(self, img, compression_factor):
+        dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
+        dft_shift = np.fft.fftshift(dft)
 
+        threshold = np.percentile(np.abs(dft_shift), compression_factor)
+        dft_shift_compressed = dft_shift * (np.abs(dft_shift) > threshold)
+
+        f_ishift = np.fft.ifftshift(dft_shift_compressed)
+        img_back = cv2.idft(f_ishift)
+        img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])
+
+        return img_back, dft_shift, dft_shift_compressed
+
+    def compression_ratio(self, dft_shift, dft_shift_compressed):
+        non_zero_before = np.count_nonzero(dft_shift)
+        non_zero_after = np.count_nonzero(dft_shift_compressed)
+
+        return non_zero_before / non_zero_after
+
+    def show_compressed_image(self, compression_factor):
+        (R, G, B) = cv2.split(self.img)
+        compressed_R, dft_shift_R, dft_shift_compressed_R = self.compress_image(R, compression_factor)
+        compressed_G, dft_shift_G, dft_shift_compressed_G = self.compress_image(G, compression_factor)
+        compressed_B, dft_shift_B, dft_shift_compressed_B = self.compress_image(B, compression_factor)
+
+        compressed_img = cv2.merge([compressed_R, compressed_G, compressed_B])
+
+        # Normalize the pixel values to the range [0, 1]
+        compressed_img_normalized = cv2.normalize(compressed_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
+                                                  dtype=cv2.CV_32F)
+
+        compression_ratio_R = self.compression_ratio(dft_shift_R, dft_shift_compressed_R)
+        compression_ratio_G = self.compression_ratio(dft_shift_G, dft_shift_compressed_G)
+        compression_ratio_B = self.compression_ratio(dft_shift_B, dft_shift_compressed_B)
+
+        average_compression_ratio = (compression_ratio_R + compression_ratio_G + compression_ratio_B) / 3
+
+        plt.imshow(compressed_img_normalized)
+        plt.title(
+            f"Compression Factor: {compression_factor}%\nAverage Compression Ratio: {average_compression_ratio:.2f}")
+        plt.show()
 
 Bonk = DFT('Bonk.jpeg')
 
@@ -114,3 +154,6 @@ Bonk = DFT('Bonk.jpeg')
 #Bonk.showHPFTransform(1)
 #Bonk.showLPFTransform(1)
 Bonk.showColorHPFTransform(0,10)
+
+compression_factor = 99
+Bonk.show_compressed_image(compression_factor)
